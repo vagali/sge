66677aa7442ec358b8b9b7f54b4a21a662d6a2a5
2dam <2dam@HZ341352.TartangaLH.eus>
2020-01-15 13:38:02 +0100

    No pone el menu item

--- __manifest__.py
+++ __manifest__.py
@@ -29,13 +29,10 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-        'views/apunte.xml',
+        'views/apunte.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
         'demo/nivel.xml'
     ],
 }
